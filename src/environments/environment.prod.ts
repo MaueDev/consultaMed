2a8b9065039e6b0bbe5f1e17e4978461388f0d7f
MaueDev <mauricio.tecnologia@troupebrasil.com.br>
2022-11-29 18:24:26 -0300

    Corrigindo ambiente

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "ap",
+    authDomain: "ap",
+    projectId: "ap",
+    storageBucket: "ap",
+    messagingSenderId: "ap",
+    appId: "ap",  
+    measurementId: "ap",
+    databaseURL: "ap",
+  agora: {
+    appId: 'ap'
+  }
+  }
+
+};
+
